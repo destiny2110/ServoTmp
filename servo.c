8963d56601691692035580aa7ce96df7fef362ee
CuongTon <destiny2110.ttc@gmail.com>
2019-11-17 15:39:43 +0700

    Modifing servo behaviour

--- servo.c
+++ servo.c
@@ -32,6 +32,7 @@ const int OnHour3 = 14;
 const int OnMin3 = 16;
 const int OffHour3 = 14;
 const int OffMin3 = 17;
+
 void setup() {
   Serial.begin(115200);
   rtc.begin();
@@ -57,34 +58,30 @@ void loop() {
   Serial.println(" ");
   delay (1000);
   
-  if((t.hour == OnHour && t.min == OnMin) || 
-     (t.hour == OnHour2 && t.min == OnMin2) ||
-     (t.hour == OnHour3 && t.min == OnMin3)){
+  if( (t.hour == OnHour  && t.min == OnMin ) || 
+      (t.hour == OnHour2 && t.min == OnMin2) ||
+      (t.hour == OnHour3 && t.min == OnMin3)   ) {
+    // If ON time is reached
+    // Relay switched ON
     digitalWrite(6,HIGH);
     digitalWrite(7,HIGH);
-    servo_9.write(45); // write to servo
+    // Servo rotate 90 degree
+    servo_9.write(90);
+    // Print some thing to console
     Serial.println("Den gio an");
-    }
-    
-  else if((t.hour == OffHour && t.min == OffMin) ||
-          (t.hour == OffHour2 && t.min == OffMin2)||
-          (t.hour == OffHour3 && t.min == OffMin3)
-  ){
+  }
+  else if( (t.hour == OffHour  && t.min == OffMin ) ||
+           (t.hour == OffHour2 && t.min == OffMin2) ||
+           (t.hour == OffHour3 && t.min == OffMin3)   ) {
+      // If of timer is reached
+      // Relay switched OFF 
       digitalWrite(6,LOW);
       digitalWrite(7,LOW);
-      servo_9.write(0); // write to servo
+      // Servo rotate to 0 degreee
+      servo_9.write(0);
+      // Print some thing to console
       Serial.println("Het gio an");
-    }
-
-
-    servo_9.write(45); // write to servo
-    _delay(1);
-    servo_9.write(0); // write to servo
-    _delay(1);
-    
-    _loop(1);
-
-
+  }
 }
 
 
@@ -92,8 +89,4 @@ void loop() {
 void _delay(float seconds){
     long endTime = millis() + seconds * 1000;
     while(millis() < endTime)_loop();
-}
-
-void _loop(){
-    
 }
